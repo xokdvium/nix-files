c7f74ca0d5cc424414492d221e804f9a37f05a62
Sergei Zimmerman <xokdvium@proton.me>
2023-12-20 16:27:03 +0300

    [starship]: Set up starship prompt instead of russel

--- home/modules/headless/zsh.nix
+++ home/modules/headless/zsh.nix
@@ -9,24 +9,25 @@
     navi
   ];
 
-  programs.zsh = {
-    enable = true;
-
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    historySubstringSearch.enable = true;
-    syntaxHighlighting.enable = true;
-
-    oh-my-zsh = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = ["git" "thefuck"];
-      theme = "robbyrussell";
-    };
 
-    # FIXME: Hacky workaround to make Gnome accept session variables
-    envExtra = ''
-      EDITOR=${config.programs.helix.package}/bin/hx
-    '';
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      historySubstringSearch.enable = true;
+      syntaxHighlighting.enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["git" "thefuck"];
+      };
+
+      # FIXME: Hacky workaround to make Gnome accept session variables
+      envExtra = ''
+        EDITOR=${config.programs.helix.package}/bin/hx
+      '';
+    };
   };
 
   home.persistence."/persistent/home/${config.home.username}" = lib.mkIf config.extraOptions.persistence.enable {
