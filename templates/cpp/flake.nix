2cad71c22c1f940e4548c26f6d30dc9beb7476ef
Sergei Zimmerman <xokdvium@proton.me>
2023-12-20 22:42:44 +0300

    [cpp]: Add linting phase to cpp template

--- templates/cpp/flake.nix
+++ templates/cpp/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    lint-nix.url = "github:xc-jp/lint.nix";
 
     mini-compile-commands = {
       url = "github:danielbarter/mini_compile_commands";
@@ -23,20 +24,59 @@
         overlays = [];
       };
 
+      clang-tools = pkgs.clang-tools_17;
+      lints = inputs.lint-nix.lib.lint-nix {
+        inherit pkgs;
+        src = ./.;
+
+        linters = {
+          ruff = {
+            ext = ".py";
+            cmd = "${pkgs.ruff}/bin/ruff $filename";
+          };
+        };
+
+        formatters = {
+          clang-format = {
+            ext = [".c" ".cpp" ".h" ".hpp" ".cc"];
+            cmd = "${clang-tools}/bin/clang-format";
+            stdin = true;
+          };
+
+          black = {
+            ext = ".py";
+            cmd = "${pkgs.black}/bin/black $filename";
+          };
+        };
+      };
+
       devShell =
         (pkgs.mkShell.override {
-          stdenv = pkgs.llvmPackages_16.stdenv;
+          stdenv = pkgs.llvmPackages_17.stdenv;
         }) {
           nativeBuildInputs = with pkgs; [
             cmake
             python311
             doxygen
             ninja
+            clang-tools
+            act
           ];
         };
     in {
       devShells = {
         default = devShell;
       };
+
+      packages = {
+        inherit
+          (lints)
+          all-checks
+          all-formats
+          all-lints
+          ;
+      };
+
+      formatter = pkgs.alejandra;
     });
 }
