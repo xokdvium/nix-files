e9ce9d6554deb4d6734a04cebf1c0afde3fc54ec
Sergei Zimmerman <xokdvium@proton.me>
2023-12-24 12:59:05 +0300

    [nixpkgs-update]: Add substituters

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -30,12 +30,14 @@ in {
         "https://helix.cachix.org/"
         "https://cache.nixos.org/"
         "https://nixpkgs-wayland.cachix.org"
+        "https://nixpkgs-update.cachix.org/"
       ];
 
       trusted-public-keys = [
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
       ];
     };
 
