6e9a274b580abb5fc2190170b4ccbf2f1ecb5d79
Sergei Zimmerman <xokdvium@proton.me>
2023-12-26 14:57:20 +0300

    [misc]: Fix formatting and enable systemd-resolve

--- hosts/features/zerotier/default.nix
+++ hosts/features/zerotier/default.nix
@@ -1,8 +1,14 @@
 _: let
   networkId = "a09acf023353c355";
 in {
-  services.zerotierone = {
-    enable = true;
-    joinNetworks = [networkId];
+  services = {
+    zerotierone = {
+      enable = true;
+      joinNetworks = [networkId];
+    };
+
+    resolved = {
+      enable = true;
+    };
   };
 }
