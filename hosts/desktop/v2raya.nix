7e759031aea85aa8744ae0f370c3944bc0639f01
Sergei Zimmerman <serjtsimmerman@gmail.com>
2024-02-05 21:20:06 +0300

    [v2raya]: Enable persistence
    Enable persistence for v2raya client vray client.

--- hosts/desktop/v2raya.nix
+++ hosts/desktop/v2raya.nix
@@ -1,5 +1,13 @@
-_: {
+{
+  lib,
+  config,
+  ...
+}: {
   services.v2raya = {
     enable = true;
+  };
+
+  environment.persistence."/state" = lib.mkIf config.xokdvium.nixos.persistence.enable {
+    directories = ["/etc/v2raya"];
   };
 }
