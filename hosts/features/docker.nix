4ae9baad6ed6a0ca5bd67339f5f79b2ae1e73cae
Sergei Zimmerman <serjtsimmerman@gmail.com>
2024-02-11 22:59:12 +0300

    [misc]: Bump deps and fixup state
    Disable docker prune for desktop systems. Temporarily
    override base16-schemes repo source, since stylix does not
    support the new repo yet.

--- hosts/features/docker.nix
+++ hosts/features/docker.nix
@@ -5,11 +5,6 @@
 }: {
   virtualisation.docker = {
     enable = true;
-    autoPrune = {
-      enable = true;
-      dates = "hourly";
-      flags = ["--all"];
-    };
   };
 
   environment.persistence."/state" = lib.mkIf config.xokdvium.nixos.persistence.enable {
--- modules/home-manager/desktop/obsidian.nix
+++ modules/home-manager/desktop/obsidian.nix
@@ -26,7 +26,7 @@ in {
       ];
 
       home.persistence."/persistent/home/${config.home.username}" = lib.mkIf config.xokdvium.home.persistence.enable {
-        directories = [".config/Obsidian"];
+        directories = [".config/obsidian"];
       };
     };
 }
