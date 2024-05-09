ccfc9ec07c7e26719738f7629026406ecbeefe4d
Sergei Zimmerman <serjtsimmerman@gmail.com>
2024-05-09 21:26:45 +0300

    feat(crypto): add gpg/pcscd workaround

--- modules/home-manager/crypto/gpg.nix
+++ modules/home-manager/crypto/gpg.nix
@@ -21,7 +21,23 @@ in
       cfg = config.xokdvium.home.crypto.gpg;
     in
     lib.mkIf cfg.enable {
+
       programs.gpg = {
+        package = pkgs.gnupg.override {
+          # TODO: Remove when PR 308884 is available
+          pcsclite = pkgs.pcsclite.overrideAttrs (old: {
+            postPatch =
+              old.postPatch
+              + (lib.optionalString
+                (!(lib.strings.hasInfix ''--replace-fail "libpcsclite_real.so.1"'' old.postPatch))
+                ''
+                  substituteInPlace src/libredirect.c src/spy/libpcscspy.c \
+                    --replace-fail "libpcsclite_real.so.1" "$lib/lib/libpcsclite_real.so.1"
+                ''
+              );
+          });
+        };
+
         enable = true;
         publicKeys = [
           {
