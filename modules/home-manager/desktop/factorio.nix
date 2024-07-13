07949202076ab0f7eee9e7ee520f1a8dfa8240cf
Sergei Zimmerman <serjtsimmerman@gmail.com>
2024-07-13 16:16:52 +0300

    feat(factorio): add helmod mod

--- modules/home-manager/desktop/factorio.nix
+++ modules/home-manager/desktop/factorio.nix
@@ -20,10 +20,29 @@ in
   config =
     let
       cfg = config.xokdvium.home.desktop.factorio;
+      modDrv = pkgs.factorio-utils.modDrv {
+        allRecommendedMods = true;
+        allOptionalMods = false;
+      };
+
+      helmod =
+        let
+          version = "0.12.20";
+          name = "helmod";
+        in
+        modDrv {
+          src = pkgs.fetchurl {
+            url = "https://github.com/Helfima/${name}/archive/refs/tags/${version}.zip";
+            hash = "sha256-m7Mdnmk0koZbv3TDoXDVkjB3ZjdM+Ew96kFsSSEDdNQ=";
+            name = "${name}_${version}.zip";
+          };
+        };
+
+      factorioWithMods = pkgs.factorio.override { mods = [ helmod ]; };
     in
     lib.mkIf cfg.enable {
       home = {
-        packages = with pkgs; [ factorio ];
+        packages = [ factorioWithMods ];
         persistence = {
           "/persistent/home/${config.home.username}" = lib.mkIf config.xokdvium.home.persistence.enable {
             directories = [ ".factorio" ];
