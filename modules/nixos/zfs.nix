b53263f29a8a2dd481b37170b55028756b4e245c
Sergei Zimmerman <145775305+xokdvium@users.noreply.github.com>
2024-09-21 23:35:47 +0300

    refactor(zfs): drop usage of latestCompatibleLinuxPackages
    Because it has been deprecated and points to the default kernel.

--- modules/nixos/zfs.nix
+++ modules/nixos/zfs.nix
@@ -46,10 +46,7 @@ in
   config = lib.mkIf cfg.enable {
     boot = {
       supportedFilesystems = [ "zfs" ];
-
-      kernelPackages = lib.mkDefault config.boot.zfs.package.latestCompatibleLinuxPackages;
       kernelParams = [ "zfs.zfs_arc_max=${builtins.toString cfg.arcSize}" ];
-
       zfs.devNodes = "/dev/disk/by-partuuid";
     };
 
