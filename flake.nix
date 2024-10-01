902e849b4876bbf5a4f25edd61f4fd9563c71f72
Sergei Zimmerman <145775305+xokdvium@users.noreply.github.com>
2024-10-01 13:15:35 +0300

    fix(flake): add extra-trusted-public-keys for ci

--- flake.nix
+++ flake.nix
@@ -115,6 +115,7 @@
 
   nixConfig = {
     extra-substituters = [ "https://helix.cachix.org" ];
+    extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
   };
 
   outputs =
