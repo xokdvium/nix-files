12b41eb17cf357eac46b7348a0e51026881031c5
Sergei Zimmerman <145775305+xokdvium@users.noreply.github.com>
2024-10-03 16:29:03 +0300

    chore: bump nix back up to 2.24

--- modules/home-manager/headless/core.nix
+++ modules/home-manager/headless/core.nix
@@ -1,10 +1,26 @@
-{ config, extraConfig, ... }:
+{
+  config,
+  extraConfig,
+  lib,
+  ...
+}:
+
 let
   inherit (extraConfig) users;
   user = builtins.getAttr config.home.username users;
 in
+
 {
   imports = [ ../../../hosts/base/nixpkgs.nix ];
+
+  # NOTE: Because being a trusted-user is really bad, this config has moved away from that.
+  # But some home-manager modules really like to add their own substituters. This
+  # leads to nix spamming you with warnings. Doesn't look like this actually removes
+  # any warnings, but this at least explicitly dumps all custom substituters from user config.
+  nix.settings = {
+    extra-substituters = lib.mkForce [ ];
+    extra-trusted-public-keys = lib.mkForce [ ];
+  };
 
   home = {
     homeDirectory = user.homePath;
